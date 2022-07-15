dc7d1eaa30c20a7444eb94e3cc87e4b6b9cbd44a
mdawood832 <mdawood832@gmail.com>
2022-07-15 13:56:06 -0500

    finished cards

--- js/app.js
+++ js/app.js
@@ -1,35 +1,61 @@
 class Game {
     constructor(){
-        this.time = time
+        this.time = 100
         this.flips = 10
-        this.score = score
+        this.score = 0
     }
-
-    
-
 
     RandomCards () {
         // let cards = document.querySelectorAll('.card')
+        let shuffledCards = imageArray.sort(()=> Math.random() - 0.5)
+        console.log(shuffledCards)
+        // for(let i = 0; i < imageArray.length; i++){
+        //     imageArray[i](()=> Math.random() - 0.5)
+        // }
         
     }
 
     FlipCard () {
-
+        // console.log("card flipped"), console logs and works fine
+        // console.log(this), this is the matchbox card
+        this.classList.toggle('flip')
+        let flips = this.flips
+        flips = document.querySelector('.flips')
+        flips--
+        console.log(flips)
     }
-
-
-
-
 
 
 
 }
 
-Game.time = document.querySelector('.time')
-Game.flips = document.querySelector('.flips')
-Game.score = document.querySelector('.score')
+//creating an array for all the cards 
+const imageArray = []
 
-const images = () =>  [
-{}
+imageArray[1] = document.querySelector('.tripleSeven').src="images/wallpaper.jpeg" 
+imageArray[1] = document.querySelector('.tripleSeven').src="images/wallpaper.jpeg" 
+imageArray[2] = document.querySelector('.doNotFear').src="images/doNotFearChange.JPG"
+imageArray[3] = document.querySelector('.doNotFear').src="images/doNotFearChange.JPG"
+imageArray[4] = document.querySelector('.IamBetter').src="images/iAmBetter.JPG"
+imageArray[5] = document.querySelector('.IamBetter').src="images/iAmBetter.JPG"
+imageArray[6] = document.querySelector('.IfYouChange').src="images/ifYouChange.JPG"
+imageArray[7] = document.querySelector('.IfYouChange').src="images/ifYouChange.JPG"
+imageArray[8] = document.querySelector('.enough').src="images/youAreEnough.JPG"
+imageArray[9] = document.querySelector('.enough').src="images/youAreEnough.JPG"
+imageArray[10] = document.querySelector('.skeletons').src="images/skeletons.JPG"
+imageArray[11] = document.querySelector('.skeletons').src="images/skeletons.JPG"
+imageArray[12] = document.querySelector('.beKind').src="images/beKindToYourself.JPG"
+imageArray[13] = document.querySelector('.beKind').src="images/beKindToYourself.JPG"
+imageArray[14] = document.querySelector('.moon').src="images/theMoon.JPG"
+imageArray[15] = document.querySelector('.moon').src="images/theMoon.JPG"
 
-]
+// console.log(imageArray)
+
+const player = new Game()
+player.RandomCards();
+player.time = document.querySelector('.time')
+player.score = document.querySelector('.score')
+
+const matchBoxCard = document.querySelectorAll('.match-box-card')
+
+matchBoxCard.forEach(card => card.addEventListener('click', player.FlipCard))
