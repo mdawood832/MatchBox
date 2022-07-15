8383a556fde8d1ef5e6264bb01b8e645b479f5c0
mdawood832 <mdawood832@gmail.com>
2022-07-15 11:58:56 -0500

    current

---
+++ js/app.js
@@ -0,0 +1,35 @@
+class Game {
+    constructor(){
+        this.time = time
+        this.flips = 10
+        this.score = score
+    }
+
+    
+
+
+    RandomCards () {
+        // let cards = document.querySelectorAll('.card')
+        
+    }
+
+    FlipCard () {
+
+    }
+
+
+
+
+
+
+
+}
+
+Game.time = document.querySelector('.time')
+Game.flips = document.querySelector('.flips')
+Game.score = document.querySelector('.score')
+
+const images = () =>  [
+{}
+
+]
