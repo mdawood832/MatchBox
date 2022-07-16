2912081ea5eac3e66995c5fe192b482ab4efb67b
mdawood832 <mdawood832@gmail.com>
2022-07-16 12:08:21 -0500

    finished flipping and currently working on timer

--- js/app.js
+++ js/app.js
@@ -1,41 +1,59 @@
 const matchBoxCard = document.querySelectorAll('.match-box-card')
+matchBoxCard.forEach(card => card.addEventListener('click', FlipCard))
+// ----------------------------------------------------------------------------------->
 
-class Game {
-    constructor(){
-        this.time = 100
-        this.flips = 10
-        this.score = 0
-    }
 
-    RandomCards () {
-    //   matchBoxCard.forEach(card => {
-    //     let position = Math.floor(Math.random()*16)
-    //     card.document.style.order = position})
-    }
+// ----------------------------------------------------------------------------------->
+function RandomCards () {
+//   matchBoxCard.forEach(card => {
+//     let position = Math.floor(Math.random()*16)
+//     card.document.style.order = position})
+}
         
-    
+ // ----------------------------------------------------------------------------------->   
+ let flips = 10
+ flips = document.querySelector('.flips')
 
-    FlipCard () {
-        // console.log("card flipped"), console logs and works fine
-        // console.log(this), this is the matchbox card
-        this.classList.toggle('flip')
-        let flips = this.flips
-        flips = document.querySelector('.flips')
-        flips--
-        console.log(flips)
-    }
-
-
-
+ function FlipCard () {
+    // console.log("card flipped"), console logs and works fine
+    // console.log(this), this is the matchbox card
+    this.classList.toggle('flip')
+    flips--
+    console.log(flips)
 }
 
+// ----------------------------------------------------------------------------------->
+function startGame() {
+    let start = document.querySelector('.startGamebtn')
+    start.addEventListener('click' , () => {
+        console.log("clicked")
+        setInterval(timer,1000)
+        if (totalTime === 0){
+            clearInterval(timer)
+        }
+    })
+}
 
+// ----------------------------------------------------------------------------------->
 
-const player = new Game()
-player.RandomCards();
-player.time = document.querySelector('.time')
-player.score = document.querySelector('.score')
+// let time = document.querySelector('.time')
+// totalTime = 10;
+let time = document.querySelector('.time')
+totalTime = 10;
+if (totalTime === 0){
+    clearInterval(timerInterval)
+}
+
+function timer() {
+    totalTime --
+    time.innerHTML = `Time: ${totalTime}`
+}
+
+// const player = new Game()
+// player.RandomCards();
+// player.time = document.querySelector('.time')
+// player.score = document.querySelector('.score')
 
 // const matchBoxCard = document.querySelectorAll('.match-box-card')
 
-matchBoxCard.forEach(card => card.addEventListener('click', player.FlipCard))
+matchBoxCard.forEach(card => card.addEventListener('click', FlipCard()))
