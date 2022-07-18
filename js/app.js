4c1caa5af51b1d2fe26e621c03d1afa722f79ff2
mdawood832 <mdawood832@gmail.com>
2022-07-18 11:46:35 -0500

    shuffled cards

--- js/app.js
+++ js/app.js
@@ -1,90 +1,58 @@
 const matchBoxCard = document.querySelectorAll('.match-box-card')
 matchBoxCard.forEach(card => card.addEventListener('click', FlipCard))
 // ----------------------------------------------------------------------------------->
-
-
+// creating an array for all the cards 
+allCards = Array.from(matchBoxCard)
+// console.log(allCards)
 // ----------------------------------------------------------------------------------->
-function RandomCards () {
-//   matchBoxCard.forEach(card => {
-//     let position = Math.floor(Math.random()*16)
-//     card.document.style.order = position})
+// SHUFFLING CARDS 
+function RandomCards (arr) {
+  arr.forEach(card => {
+    let position = Math.floor(Math.random()*16)
+    card.style.order = position
+    console.log(card)})
 }
         
-// ----------------------------------------------------------------------------------->   
+// -----------------------------------------------------------------------------------> 
+// FLIPPING CARD AND ITTERATING FLIPS  
 let flips = 10
 flips = document.querySelector('.flips')
 
 function FlipCard () {
-    // console.log("card flipped"), console logs and works fine
-    // console.log(this), this is the matchbox card
     this.classList.toggle('flip')
-    flips--
-    console.log(flips)
+    // flips--
+    // console.log(flips)
 }
-// ----------------------------------------------------------------------------------->   
+// ----------------------------------------------------------------------------------->
+// TIMER   
 totalTime = 101;
 let time = document.querySelector('.time')
 
 function timer() {
     totalTime--
-    time.innerHTML = `Time: ${totalTime}`
-
-    // setInterval(timer,1000)
-
-    // if (totalTime === 0){
-    //     clearInterval(timerInterval)
-    // } 
+    time.innerHTML = `Time ${totalTime}`
 }
 
-// function stopTimer() {
-//     clearInterval(timerInterval)
-// }
-
-// setTimeout(stopTimer, 10000)
-
 // ----------------------------------------------------------------------------------->
+// START GAME FUNCTION
 function startGame() {
     let start = document.querySelector('.startGamebtn')
     start.addEventListener('click' , () => {
         console.log("clicked")
+        // timer function 
         timer()
         const timerInterval = setInterval(timer,1000)
 
         function stopTimer() {
             clearInterval(timerInterval)
         }
-        
         setTimeout(stopTimer, 100000)
+
+        // shuffle function 
+        RandomCards(allCards)
     })
 }
 
 // ----------------------------------------------------------------------------------->
-// totalTime = 10;
-// let time = document.querySelector('.time')
 
-// function timer() {
-//     totalTime--
-//     time.innerHTML = `Time: ${totalTime}`
-
-//     let timerInterval =setInterval(timer,1000)
-
-//     if (totalTime === 0){
-//         clearInterval(timerInterval)
-//         time.innerHTML = ''
-//     } 
-// }
-
-// function stopTimer() {
-
-// }
-
-// let timerInterval =setInterval(timer,1000)
-
-
-
-
-
-
-
-// const matchBoxCard = document.querySelectorAll('.match-box-card')
 
